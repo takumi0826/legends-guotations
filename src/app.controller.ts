4d2bf063888610009ad5fee7c03328fa2f25cf29
takumi0826 <takumi.s8.26@gmail.com>
2022-08-13 12:37:09 +0900

    volumes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('legends-guotations')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/get')
+  @Get()
   getAll(): string {
     return this.appService.getAll();
   }
