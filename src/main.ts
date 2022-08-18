b9a07ad4403bfc0626d33b1e0a205e752c5429f6
takumi0826 <takumi.s8.26@gmail.com>
2022-08-18 16:24:43 +0900

    http

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpService } from '@nestjs/axios';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,12 @@ async function bootstrap() {
     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
     methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
   });
+
+  const http = new HttpService();
+  setInterval(() => {
+    http.get('https://salty-sands-26486.herokuapp.com/');
+  }, 15 * 60 * 1000);
+
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
